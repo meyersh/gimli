2bd243c312bfa7ab56bfa97e9bb911404c72a912
meyersh <meyersh@24268613-4aff-4948-b4aa-51f13608ddc2>
2010-11-10 23:07:21 +0000

    Added more to the CPP, it's almost done. I think most of the work will be on the user interface piece.
    git-svn-id: svn+ssh://gimli.morningside.edu/home/meyersh/svn/src@9 24268613-4aff-4948-b4aa-51f13608ddc2

--- 330/Projects/5 (microcode generator)/330-p5.cpp
+++ 330/Projects/5 (microcode generator)/330-p5.cpp
@@ -10,49 +10,166 @@
 
 using namespace std;
 
+typedef unsigned long long int ulli;
+
 /*
  Instruction layout:
 
- [ NEXT INSTR (10) ] [ JMP (4) ] [ ALU CONTR (8) ] [ ZONE RD (4) ] [ REGISTER RD (20) ] [ PRM MEM (4) ]
- [ MAD SRC (4) ] [MDB SRC (4) ] [ B SRC (4) ] [ A SRC (4) ]
+ [ NEXT INSTR (10 p.64) ] [ JMP (4 p.54) ] [ ALU CONTR (8 p.50) ] [ ZONE RD (4 p.42) ] [ REGISTER RD (20 p.38) ] 
+ [ PRM MEM (2 p.18) ] [ MAD SRC (4 p.16) ] [ MDB SRC (4 p.12) ] [ B SRC (4 p.8) ] [ A SRC (4 p.4) ]
+*/
+
+/* The webpage should have fields for the following:
+
+ * Next Instruction (accept binary or decimal... first character should be a 
+   B or D to indicate which)
+
+ * JMP (4 binary digits)
+
+ * ALU Control (8 binary digits)
+
+ * Zone Read (4 bits)
+
+ * Register Read (10 fields, each labeled for a register, 2 binary digits per 
+   field)
+
+ * Primary Memory (2 bits)
+
+ * MAD source ( accept binary or decimal... first character should be a B or D 
+   to indicate which)
+
+ * MDB source ( accept binary or decimal... first character should be a B or D 
+   to indicate which)
+
+ * B source ( accept binary or decimal... first character should be a B or D 
+   to indicate which)
+
+ * A  source ( accept binary or decimal... first character should be a B or D 
+   to indicate which)
+
+ * For JMP, ALU control, Zone Read, each of the Register Read subfields, and Primary Memory 
+ you may use individual bit labels w/ radio buttons, check boxes, and/or select lists if you prefer.
 
 */
 
 
-unsigned long long int d_str_to_binary(string inpt)
+
+*/
+
+/* Microinstruction format positions: */
+enum {
+  NEXT_INSTR = 64,
+  JMP = 54,
+  ALU_CONTR = 50,
+  ZONE_RD = 42,
+  REG_RD = 38,
+  PRM_MEM = 18,
+  MAD_SRC = 16,
+  MDB_SRC = 12,
+  B_SRC = 8,
+  A_SRC = 4};
+
+/*
+ * binary_to_str(ulli) - given a ulli representation of a number, 
+ * return a string of that binary.
+ */
+
+string binary_to_str(ulli binary)
+{
+  string ret;
+  for (ulli i = ((ulli)1 << 63); i; i >>= 1)
+    {
+      ret += (binary & i) ? '1' : '0';
+    }
+  return ret;
+}
+
+/* 
+ * d_str_to_binary(string) - given a string with a leading D (for decimal)
+ * return a ulli representing that number.
+ */
+
+ulli d_str_to_binary(string inpt)
 {
   
   /* erase first character [ B | D ] */
   inpt.erase(inpt.begin());
   
-  return (unsigned long long int)atoi( inpt.c_str() );
+  return (ulli)atoi( inpt.c_str() );
 
 }
 
-unsigned long long int b_str_to_binary(string inpt)
+/*
+ * b_str_to_binary(string) - given a string with a leading B (for binary)
+ * return a ulli representing that number. 
+ */
+
+ulli b_str_to_binary(string inpt)
 {
   /* erase first character [ B | D ] */
   inpt.erase(inpt.begin());
   
-  unsigned long long int binary = 0;
+  ulli binary = 0;
 
   for (int i = 0; i < inpt.size(); i++)
     {
+      binary <<= 1;
       if (inpt[i] == '1')
 	binary += 1;
-      binary <<= 1;
     }
 
   return binary;
 }
+
+ulli str_to_binary(string inpt)
+{
+  switch ( toupper(inpt[0]) ) 
+    {
+    case 'D':
+      return d_str_to_binary(inpt);
+      break;
+      
+    case 'B':
+      return b_str_to_binary(inpt);
+      break;
+      
+    default:
+      return 0;
+    }
+}
+
+
+/*
+  given something like '000011' if we want this entire thing moved to a given
+  location, where the leading 0 would be at `pos` we simply left shift by 
+  (pos-len).
+  
+  pos = position. 0 is the least significant (right-most) bit
+  len = length of our input binary.
+*/
+
+ulli set_binary_pos(ulli binary, int pos, int len)
+{
+  return binary << (pos-len);
+}
+	   
+      
 
 int main()
 {
 
   /* inputs */
 
-  string s_next_instruction, s_jmp, s_alu_control, s_zone_read, s_register_read,
-    s_primary_memory, s_MAD_source, s_MDB_source, s_B_source, s_A_source;
+  string s_next_instruction, 
+    s_jmp, 
+    s_alu_control, 
+    s_zone_read, 
+    s_register_read,
+    s_primary_memory,
+    s_MAD_source, 
+    s_MDB_source, 
+    s_B_source,
+    s_A_source;
 
   /*
   cin >> s_next_instruction 
@@ -67,10 +184,13 @@ int main()
       >> s_A_source;
   
   */
-  cin >> s_B_source >> s_A_source;
+  cin >> s_A_source;
+  ulli binary = str_to_binary(s_A_source);
 
-  cout << b_str_to_binary(s_B_source) << endl 
-       << d_str_to_binary(s_A_source) << endl;
+  cout << binary << endl
+       << binary_to_str(binary) << endl
+       << binary_to_str(set_binary_pos(15, 4, 4)) << endl
+       << binary_to_str(set_binary_pos(15, 64, 4)) << endl;
 
   return 0;
 
