7c490338319bae8c547f8c334bc7167b4685707d
meyersh <meyersh@24268613-4aff-4948-b4aa-51f13608ddc2>
2010-11-10 19:33:04 +0000

    Binary & Decimal converters are working. `unsigned long long int` gives us 64-bits to play with.
    git-svn-id: svn+ssh://gimli.morningside.edu/home/meyersh/svn/src@8 24268613-4aff-4948-b4aa-51f13608ddc2

--- 330/Projects/5 (microcode generator)/330-p5.cpp
+++ 330/Projects/5 (microcode generator)/330-p5.cpp
@@ -6,7 +6,9 @@
 
 #include <iostream>
 #include <string>
-#include <ccstring>
+#include <cstdlib>
+
+using namespace std;
 
 /*
  Instruction layout:
@@ -16,21 +18,43 @@
 
 */
 
+
+unsigned long long int d_str_to_binary(string inpt)
+{
+  
+  /* erase first character [ B | D ] */
+  inpt.erase(inpt.begin());
+  
+  return (unsigned long long int)atoi( inpt.c_str() );
+
+}
+
+unsigned long long int b_str_to_binary(string inpt)
+{
+  /* erase first character [ B | D ] */
+  inpt.erase(inpt.begin());
+  
+  unsigned long long int binary = 0;
+
+  for (int i = 0; i < inpt.size(); i++)
+    {
+      if (inpt[i] == '1')
+	binary += 1;
+      binary <<= 1;
+    }
+
+  return binary;
+}
+
 int main()
 {
 
   /* inputs */
-  string s_next_instruction, 
-    s_jmp,
-    s_alu_control,
-    s_zone_read,
-    s_register_read,
-    s_primary_memory,
-    s_MAD_source,
-    s_MDB_source,
-    s_B_source,
-    s_A_source;
 
+  string s_next_instruction, s_jmp, s_alu_control, s_zone_read, s_register_read,
+    s_primary_memory, s_MAD_source, s_MDB_source, s_B_source, s_A_source;
+
+  /*
   cin >> s_next_instruction 
       >> s_jmp 
       >> s_alu_control
@@ -41,9 +65,12 @@ int main()
       >> s_MDB_source
       >> s_B_source
       >> s_A_source;
+  
+  */
+  cin >> s_B_source >> s_A_source;
 
-
-    
+  cout << b_str_to_binary(s_B_source) << endl 
+       << d_str_to_binary(s_A_source) << endl;
 
   return 0;
 
