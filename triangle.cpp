dae72c39cad596462fb33442fab1e9b629106bfb
meyersh <meyersh@24268613-4aff-4948-b4aa-51f13608ddc2>
2010-11-12 21:46:29 +0000

    Finalizing this algorithm.
    git-svn-id: svn+ssh://gimli.morningside.edu/home/meyersh/svn/src@11 24268613-4aff-4948-b4aa-51f13608ddc2

--- triangle.cpp
+++ triangle.cpp
@@ -1,10 +1,20 @@
+/***
+ * Shaun Meyer goes for BONUS POINTS
+ **/
 #include <iostream>
-
+#include <cstdlib>
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-  int w = 4;
+  int w = 0;
+  if (argc > 1) {
+    w = atoi(argv[1]);
+  } else {
+    cout << "USAGE: " << argv[0] << " <integer>\n";
+    cout << "May I please have an integer?>> ";
+    cin >> w;
+  }
 
   int maxw = 4*w;
 
@@ -12,7 +22,6 @@ int main()
     {
       for (int c = 0; c < (w * 4); c++)
 	{
-
 	  /* insert manditory spaces */
 	  if (c && c % w == 0)
 	    cout << " ";
@@ -20,29 +29,29 @@ int main()
 	  if (c < maxw/2) { /* first half */
 	    if (c < maxw/4) { /* first quarter */
 	      if (c <= r) 
-		cout << '4';
-	      else 
 		cout << 'X';
+	      else 
+		cout << '_';
 	    }
 	    else { /* 2nd quarter */
-	      if (c < w-r+4)
-		cout << '4';
-	      else 
+	      if (c < w-r+w) 
 		cout << 'X';
+	      else 
+		cout << '_';
 	    }
 	  }
 	  else { /* 2nd half */
-	    if (c < maxw*3/4) {
+	    if (c < maxw*3/4) { /* 3rd quarter */
 	      if (c < r + w*2)
-		cout << 'X';
+		cout << '_';
 	      else
-		cout << '4';
+		cout << 'X';
 	    } 
-	    else { /* 3rd quarter */
+	    else { /* 4th quarter */
 	      if (c < w-1-r+w*3)
-		cout << 'X';
+		cout << '_';
 	      else
-		cout << '4';
+		cout << 'X';
 	    }
 	  }
 	}
