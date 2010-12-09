f5a0ab62f2fe527276b599217799df7a848c5c0c
meyersh <meyersh@24268613-4aff-4948-b4aa-51f13608ddc2>
2010-12-09 01:43:03 +0000

    Much more completed in sim.rb. It's practically done.
    git-svn-id: svn+ssh://gimli.morningside.edu/home/meyersh/svn/src@123 24268613-4aff-4948-b4aa-51f13608ddc2

--- 330/Projects/6 (mcsci)/sim.rb
+++ 330/Projects/6 (mcsci)/sim.rb
@@ -1,65 +1,202 @@
 #!/usr/bin/ruby
 #
 
-SRC = {
-  :reg0 => 0b0000,
-  :reg1 => 0b0001,
-  :reg2 => 0b0010,
-  :reg3 => 0b0011,
-  :reg4 => 0b0100,
-  :reg5 => 0b0101,
-  :reg6 => 0b0110,
-  :reg7 => 0b0111,
-  :ir => 0b1110,
-  :pc => 0b1111,
-  :zone1 => 0b1000,
-  :zone2 => 0b1001,
-  :zone3 => 0b1010,
-  :zone4 => 0b10011,
-  :zero => 0b1100,
-  :one => 0b1101
-}
+# SOURCES
+class Src 
+  REG0 = 0b0000
+  REG1 = 0b0001
+  REG2 = 0b0010
+  REG3 = 0b0011
+  REG4 = 0b0100
+  REG5 = 0b0101
+  REG6 = 0b0110
+  REG7 = 0b0111
+  IR = 0b1110
+  PC = 0b1111
+  ZONE0 = 0b1000
+  ZONE1 = 0b1001
+  ZONE2 = 0b1010
+  ZONE3 = 0b1011
+  ZERO = 0b1100
+  ONE = 0b1101
+end
 
-PRM_MEM = {
-  :access => 0b10,
-  :rw => 0b01
-}
+# BUS / MEMORY SELECTORS
+rw = 0b01
+access = 0b10
+busselect = 0b01
 
-REG_RD = {
-  :access => 0b10,
-  :busselect => 0b01
-}
+class Reg
+  class PC
+    Access = 0x80000
+    Select = 0x40000
+  end
+  class IR
+    Access = 0x20000
+    Select = 0x10000
+  end
+  class REG7
+    Access = 0x08000
+    Select = 0x04000
+  end
+  class REG6
+    Access = 0x02000
+    Select = 0x01000
+  end
+  class REG5
+    Access = 0x00800
+    Select = 0x00400
+  end
+  class REG4
+    Access = 0x00200
+    Select = 0x00100
+  end
+  class REG3
+    Access = 0x00080
+    Select = 0x00040
+  end
+  class REG2
+    Access = 0x00020
+    Select = 0x00010
+  end
+  class REG1
+    Access = 0x00008
+    Select = 0x00004
+  end
+  class REG0
+    Access = 0x00002
+    Select = 0x00001
+  end
+end
 
-JMP = {
-  :jamn => 0b1000,
-  :jamz => 0b0100,
-  :jamp => 0b0010,
-  :jamc => 0b0001
-}
+class Zone
+  ZONE3 = 0b1000
+  ZONE2 = 0b0100
+  ZONE1 = 0b0010
+  ZONE0 = 0b0001
+end
 
-ALU_SHIFT = {
-  :noshift => 0b00,
-  :a_rs => 0b01,
-  :left_shift => 0b10,
-  :right_shift => 0b11
-}
+# JAM Bits
+class Jam
+  N = 0b1000
+  Z = 0b0100
+  P = 0b0010
+  C = 0b0001
+end
 
-ALU_CNTR = {
-  :a => 0b011000,
-  :b => 0b010100,
-  :not_a => 0b011010,
-  :not_b => 0b101100,
-  :a_plus_b => 0b111100,
-  :a_plus_b_plus_1 => 0b111101,
-  :inc_a => 0b111001,
-  :inc_b => 0b110101,
-  :b_sub_a => 0b111111,
-  :dec_b => 0b110110,
-  :neg_a => 0b111011,
-  :a_and_b => 0b001100,
-  :a_or_b => 0b011100,
-  :zero => 0b010000,
-  :one =>  0b110010,
-  :neg_1 => 0b110010
-}
+# ALU Shifter
+class Shift
+  NO = 0b00
+  A_RS = 0b01
+  LEFT_SHIFT = 0b10
+  RIGHT_SHIFT = 0b11
+end
+
+# ALU Control
+class Alu
+  A = 0b011000
+  B = 0b010100
+  NOT_A = 0b011010
+  NOT_B = 0b101100
+  ADD = 0b111100
+  A_PLUS_B_PLUS_1 = 0b111101
+  INC_A = 0b111001
+  INC_B = 0b110101
+  B_SUB_A = 0b111111
+  DEC_B = 0b110110
+  NEG_A = 0b111011
+  A_AND_B = 0b001100
+  A_OR_B = 0b011100
+  ZERO = 0b010000
+  ONE =  0b110010
+  NEG_1 = 0b110010
+end
   
+class Microinstruction
+  attr_accessor :symbol, :next_instr, :jmp, :alu_shift, :alu_control, :zone_rd, :reg_rd, :prm_mem, :mad_src, :mdb_src, :b_src, :a_src
+
+  def initialize(params)
+
+    # Set default parameters to something sane(?)
+    params = {
+      :symbol => nil,
+      :next_instr => 0, 
+      :jmp => 0, 
+      :alu_shift => 0, 
+      :alu_control => 0, 
+      :zone_rd => 0, 
+      :reg_rd => 0,
+      :prm_mem => 0, 
+      :mad_src => 0, 
+      :mdb_src => 0, 
+      :b_src => 0, 
+      :a_src => 0 
+    }.merge(params)
+
+    # Set all of our attributes:
+    @symbol = params[:symbol]
+    @next_instr = params[:next_instr] & 1023 # Mask out any more than 10 bits
+    @jmp = params[:jmp]
+    @alu_shift = params[:alu_shift]
+    @alu_control = params[:alu_control]
+    @zone_rd = params[:zone_rd]
+    @reg_rd = params[:reg_rd]
+    @prm_mem = params[:prm_mem]
+    @mad_src = params[:mad_src]
+    @mdb_src = params[:mdb_src]
+    @b_src = params[:b_src]
+    @a_src = params[:a_src]
+      end
+
+  # Return a string representation (64-bit binary, in our case)
+  def binary
+    return "%010b%04b%02b%06b%04b%020b%02b%04b%04b%04b%04b" % [@next_instr, @jmp, 
+                                                          @alu_shift, @alu_control, 
+                                                          @zone_rd, @reg_rd, @prm_mem, 
+                                                          @mad_src, @mdb_src, @b_src, @a_src]
+  end
+
+  def hex
+    return "%016x" % String("0b" + self.binary)
+  end
+
+  def to_s
+    return self.binary
+  end
+
+end
+
+instructions = []
+
+instructions.push Microinstruction.new(
+                                       :symbol => "JUMP",
+                                       :a_src => Src::PC,
+                                       :alu_control => Alu::INC_A,
+                                       :reg_rd => Reg::PC::Select
+)
+
+instructions.push Microinstruction.new(
+                         :symbol => "LOAD_IMM1",
+                         :next_instr => 0b0000000001,
+                         :alu_control => Alu::A,
+                         :reg_rd => Reg::IR::Access | Reg::IR::Select,
+                         :zone_rd => Src::ZONE3
+)
+
+instructions.push Microinstruction.new(
+                                       :symbol => "ADD",
+                                       :alu_control => Alu::ADD,
+                                       :a_src => Src::ZONE0,
+                                       :b_src => Src::ZONE1,
+                                       :zone_rd => Zone::ZONE2
+)
+
+instructions.each do |mi|
+  puts mi.symbol << ":"
+  puts " => " << mi.to_s
+  puts " => " << mi.hex
+  puts
+end
+
+
+
