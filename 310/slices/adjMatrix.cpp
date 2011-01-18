3b75e570f99fa2d8c935ab4d707955356c808b61
meyersh <meyersh@24268613-4aff-4948-b4aa-51f13608ddc2>
2011-01-18 19:33:55 +0000

    Making some directories
    git-svn-id: svn+ssh://gimli.morningside.edu/home/meyersh/svn/src@139 24268613-4aff-4948-b4aa-51f13608ddc2

--- 310/slices/adjMatrix.cpp
+++ 310/slices/adjMatrix.cpp
@@ -5,7 +5,8 @@ void adjMatrix::free_matrix()
   if (matrix) 
     {
       delete [] matrix;
-      size = 0;
+      matrix_size = 0;
+      matrix = NULL;
     }
 }
 
@@ -16,19 +17,39 @@ void adjMatrix::create_matrix(int size)
    * whack it and create the requested
    * matrix (empty)
    */
-
   if (matrix)
     free_matrix();
+
+  /* 
+   * Cool, now make the new matrix and
+   * set the size
+   */
   matrix = new int[size*size];
-  this->size = size;
+  matrix_size = size;
 }
 
 void adjMatrix::resize_matrix(int new_size)
 { 
+  /* remember old matrix */
+  int * old_matrix = matrix;
+  int old_size = matrix_size;
+
+  /* make new matrix */
+  matrix = NULL; // Don't delete []! We're still using that data.
+  create_matrix(new_size);
+
+  /* Copy all the datas over */
+  for (int x = 0; x < old_size; x++)
+    for (int y = 0; y < old_size; y++)
+	edge(x, y) = old_matrix[old_size*x + y];
+    
+  /* Finally, free the old data */
+  delete [] old_matrix;
+
   return;
 }
 
-int & edge(int vertex_x, int vertex_y)
+int& adjMatrix :: edge(int vertex_x, int vertex_y)
 {
-  return matrix[size*vertex_x + vertex_y];
+  return matrix[matrix_size*vertex_x + vertex_y];
 }
--- 310/slices/adjMatrix.hpp
+++ 310/slices/adjMatrix.hpp
@@ -1,18 +1,23 @@
 #ifndef __ADJMATRIX_HPP__
+#define __ADJMATRIX_HPP__
+
+#include <cstdlib>
 
 class adjMatrix 
 {
 private:
   int *matrix, 
-    size = 0;
+    matrix_size;
   
 public:
+  int size() {return matrix_size;}
   void free_matrix();
   void create_matrix(int size);
   void resize_matrix(int new_size);
   int & edge(int vertex_x, int vertex_y);
 
-  adjMatrix() { create_matrix(10); }
+  adjMatrix() { matrix=NULL; create_matrix(10); }
+  adjMatrix(int size) {matrix=NULL; create_matrix(size);}
   ~adjMatrix() { free_matrix(); }
 
 };
