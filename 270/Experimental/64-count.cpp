a6f1be0f1379ea0df2de30e7449e65f31558a04b
meyersh <meyersh@24268613-4aff-4948-b4aa-51f13608ddc2>
2011-01-22 01:56:43 +0000

    Messing with svn:externals
    git-svn-id: svn+ssh://gimli.morningside.edu/home/meyersh/svn/src@153 24268613-4aff-4948-b4aa-51f13608ddc2

--- 270/Experimental/64-count.cpp
+++ 270/Experimental/64-count.cpp
@@ -8,7 +8,8 @@ typedef unsigned long long int ulli ;
 int main()
 {
   ulli pos = 1;
-  const ulli max = 0xFFFFFFFFFFFFFFFF;
+  const ulli max = 0xFFFFFFFF;
+#pragma omp parallel for
   for (ulli i = 0; i < max; i++)
     if (pos & i) {
       pos <<= 1;
---
+++ lib/adjmatrix/adjmatrix.hpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * adjMatrix class definition.
+ * CREATED BY: Shaun Meyer, Jan-2011
+*******************************************************************************/
+
+#ifndef __ADJMATRIX_HPP__
+#define __ADJMATRIX_HPP__
+
+#include <cstdlib>
+
+class adjMatrix 
+{
+private:
+   int *matrix, 
+      matrix_size;
+  
+public:
+   int size() {return matrix_size;}
+   void free_matrix();
+   void create_matrix(int size);
+   void resize_matrix(int new_size);
+   int & edge(int vertex_x, int vertex_y);
+
+   adjMatrix() { matrix=NULL; create_matrix(10); }
+   adjMatrix(int size) {matrix=NULL; create_matrix(size);}
+   ~adjMatrix() { free_matrix(); }
+
+};
+
+#endif
