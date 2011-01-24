9bf8f65c9aa86826a32774f3980fd2e69b67a98b
meyersh <meyersh@24268613-4aff-4948-b4aa-51f13608ddc2>
2011-01-24 01:30:13 +0000

    triary initial commit. Weird compile failure, need to show Randy.
    git-svn-id: svn+ssh://gimli.morningside.edu/home/meyersh/svn/src@161 24268613-4aff-4948-b4aa-51f13608ddc2

---
+++ lib/triary/triary.cpp
@@ -0,0 +1,40 @@
+#include "triary.hpp"
+
+template<class V>
+V& triary<V>::access(int row, int col)
+/* Unified getter+setter function for the triangle array */
+{
+    if (col > row)
+      throw std::out_of_range("Col is greater than row.");
+    if (row > num_rows)
+      throw std::out_of_range("Row is greater than number of rows.");
+
+    return triangle_array[ t(row) + col ];
+}
+
+template<class V>
+void triary<V>::resize(int rows)
+/* Set the size of the array to accomodate a
+ * given number of rows and migrate the 
+ * relevent elements */
+{
+  /* remember the old triangle */
+  int old_size = num_rows;
+  V old_array = triangle_array;
+  
+  /* Make a new triangle */
+  triangle_array = new V[t(rows)];
+  
+  int boundary;
+  if (rows > num_rows)
+    boundary = num_rows;
+  else
+    boundary = rows;
+      
+  /* Copy the old data as far as we can */
+  for (int i = 0; i < num_rows; i++)
+    triangle_array[i] = old_array[i];
+
+  delete [] old_array;
+  num_rows = rows;
+}
---
+++ lib/triary/triary.hpp
@@ -0,0 +1,34 @@
+#ifndef __TRIARY_HPP__
+#define __TRIARY_HPP__
+
+#include <cstdlib>
+#include <stdexcept>
+
+template<class V>
+class triary
+{
+private:
+  V *triangle_array;
+  int num_rows;
+  
+public:
+
+
+
+  int t(int row)  {return row*(row+1)/2;}
+  V & access(int row, int col);
+
+  void resize(int rows);
+  void free_array() {delete [] triangle_array;}
+  void create_array(int size) 
+  {
+    triangle_array = new V[t(size)];
+  }
+
+  triary() {num_rows = 0; triangle_array=NULL;}
+  triary(int rows) {num_rows = rows; create_array(rows);}
+  ~triary() {free_array();}
+
+}
+
+#endif
