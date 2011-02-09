978df538648d91da2503a26d6f778cfe3100d6d6
meyersh <meyersh@24268613-4aff-4948-b4aa-51f13608ddc2>
2011-02-09 02:36:28 +0000

    Cool little menu thing going on now.
    git-svn-id: svn+ssh://gimli.morningside.edu/home/meyersh/svn/src@217 24268613-4aff-4948-b4aa-51f13608ddc2

--- 310/slices/3/2 (unsigned char adjmatrix)/bitTest.cpp
+++ 310/slices/3/2 (unsigned char adjmatrix)/bitTest.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <unistd.h>
 #include "adjMatrix.hpp"
 #include "shaun.hpp"
 
@@ -12,6 +13,7 @@ using namespace std;
  */
 void print_help(string, string);
 unweightedGraph &read_graph_file(char *filename, bool is_symmetrical);
+void slprint(string, unsigned int delay=25000);
 
 int main(int argc, char **argv)
 {
@@ -23,14 +25,138 @@ int main(int argc, char **argv)
       exit(1);
       }
    
+   /* read in the graphs */
    unweightedGraph &directed = read_graph_file(argv[1], 1);
    unweightedGraph &undirected = read_graph_file(argv[2], 0);
+   
+   
+   unweightedGraph *cur_graph = NULL;
 
-   cout << "Directed graph:\n";
-   directed.print();
- 
-   cout << "\nUndirected graph:\n";
-   undirected.print();
+   slprint("\n\nWelcome to the graphic grapher!\n");
+
+   /* Prep for command line loop */
+   string prompt(" OK > "),
+     inpt;
+
+   while (1)
+     {
+       if (!cin.good())
+	 break;
+
+       cout << prompt;
+       std::getline(cin, inpt);
+       vector<string> cmd = split(inpt, " ");
+
+       if (cmd.size() == 1)
+	 {
+	   cmd[0] = uppercase(cmd[0]);
+	   if (cmd[0] == "QUIT")
+	     break;
+	   else if (cmd[0] == "HELP")
+	     {
+	       slprint("bitTest uses a context base system, commands affect the\n");
+	       slprint("current context, which is indicated by the prompt.\n");
+	       slprint("   COMMANDS:\n CLEAR, DIRECTED, UNDIRECTED, SET, CLEAR, ");
+	       slprint("QUIT.\n");
+	     }
+	   else if (cmd[0] == "DIRECTED")
+	     {
+	       cur_graph = &directed;
+	       prompt = " DIRECTED > ";
+	     }
+	   else if (cmd[0] == "UNDIRECTED")
+	     {
+	     cur_graph = &undirected;
+	     prompt = " UNDIRECTED > ";
+	     }
+	   else if (cmd[0] == "PRINT")
+	     cur_graph->print();
+	   else if (cmd[0] == "SET" || cmd[0] == "CLEAR")
+	     {
+	       if (cur_graph)
+		 {
+		   slprint("Doesn't look like the point is in the proper syntax.\n");
+		   slprint("Something like `" + cmd[0] + " 2,3` or `" + 
+			   cmd[0] +" ALL` would be great.\n");
+		 }
+	       else 
+		 {
+		   slprint("A DIRECTED or UNDIRECTED graph must first be selected.\n");
+		   continue;
+		 }
+	       
+	     }
+	 }
+       else if (cmd.size() == 2)
+	 {
+	   cmd[0] = uppercase(cmd[0]);
+	   cmd[1] = uppercase(cmd[1]);
+	   if (cmd[0] == "SET")
+	     {
+	       vector<string> pt = split(cmd[1]);
+
+	       if (!cur_graph)
+		 {
+		   slprint("A DIRECTED or UNDIRECTED graph must first be selected.\n");
+		   continue;
+		 }
+	       if (cmd.size() > 1 && uppercase(cmd[1]) == "ALL")
+		 {
+		   cur_graph->setAll();
+		   cout << "Graph set...\n";
+		   continue;
+		 }
+	       else if (pt.size() != 2)
+		 {
+		   slprint("Doesn't look like the point is in the proper syntax.\n");
+		   slprint("Something like `SET 2,3` `SET ALL` would be great.\n");
+		   continue;
+		 }
+
+	       int row = atoi(pt[0].c_str());
+	       int col = atoi(pt[1].c_str());
+	       
+	       cout << "Setting " << row << "," << col << endl;
+	       cur_graph->setBit(row,col);
+	     }
+	   else if (cmd[0] == "CLEAR")
+	     {
+	       vector<string> pt = split(cmd[1]);
+
+	       if (!cur_graph)
+		 {
+		   slprint("A DIRECTED or UNDIRECTED graph must first be selected.\n");
+		   continue;
+		 }
+	       if (cmd.size() > 1 && uppercase(cmd[1]) == "ALL")
+		 {
+		   cur_graph->clearAll();
+		   cout << "Graph cleared...\n";
+		   continue;
+		 }
+	       else if (pt.size() != 2)
+		 {
+		   slprint("Doesn't look like the point is in the proper syntax.\n");
+		   slprint("Something like `CLEAR 2,3` or `CLEAR ALL` would be great.\n");
+		   continue;
+		 }
+
+	       int row = atoi(pt[0].c_str());
+	       int col = atoi(pt[1].c_str());
+	       
+	       cout << "Clearing " << row << "," << col << endl;
+	       cur_graph->clearBit(row,col);
+	     }
+	 }
+       else if (cmd.size() == 3)
+	 {
+	 }
+
+       cout << "Beep!\n";
+       
+       
+     }
+
 
    cout << "All done!" << endl;
 }
@@ -100,4 +226,13 @@ unweightedGraph &read_graph_file(char *filename, bool directed_graph)
    graph_file.close();
 
    return *ret_graph;
+}
+
+void slprint(string line, unsigned int delay)
+{
+  for (int i = 0; i < line.size(); i++)
+    {
+      cout << line[i] << flush;
+      usleep(delay);
+    }
 }
