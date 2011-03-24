7e650fce16c46b7c32762aec9daca9a26d240a2b
meyersh <meyersh@24268613-4aff-4948-b4aa-51f13608ddc2>
2011-03-24 21:58:08 +0000

    Done working from work.
    git-svn-id: svn+ssh://gimli.morningside.edu/home/meyersh/svn/src@335 24268613-4aff-4948-b4aa-51f13608ddc2

--- 310/slices/8/knapsack.cpp
+++ 310/slices/8/knapsack.cpp
@@ -15,9 +15,52 @@
 
 */
 
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+struct Item {
+   unsigned int value, weight;
+
+   Item()
+   {
+      value = weight = 0;
+   }
+
+   Item(unsigned int value, unsigned int weight)
+   {
+      this->value = value;
+      this->weight = weight;
+   }
+   
+   unsigned int density() const
+   {
+      return value/weight;
+   }
+
+   bool operator< (const Item &b) const
+   {
+      if (this->density() < b.density()) 
+	 return true;
+      return false;
+   }
+   /*
+   bool operator== (const Item &b) const
+   {
+      if (this->density() == b.density())
+	 return true;
+      return false;
+   }
+   */
+};
+
 template<class V> class maxHeapPQ {
 private:
-   vector<V> heap;
+   vector<V> heap; // ptr to root of heap 
 
    void swap(V &a, V &b)
    {
@@ -40,17 +83,17 @@ private:
 	 }
    }
 
-   void sift_down(int i)
+   void sift_down(int i=0)
    {
-      if( i*2 > heap.size()-1 || i*2+1 > heap.size()-1)
+      if( i*2 > heap.size()-1 || i*2+1 > heap.size()-1 )
 	 return;
 
       int leftChild = i*2+1,
-	 rightChild = i*2+2,
+	 rightChild = i*2+2;
 
-      /* Which child is biggest ? */
-      int bc = leftChild; // default to left child.
-      if (heap[bc] < heap_ary[rightChild])
+	 /* Which child is biggest ? */
+	 int bc = leftChild; // default to left child.
+      if (heap[bc] < heap[rightChild])
 	 bc = rightChild; // right child is bigger.
       
       /* If heap[bc] > heap[i], 
@@ -68,6 +111,7 @@ public:
       heap.push_back(item);
       sift_up();
    }
+
    int readlargest(V &item) {
       if (heap.size())
 	 item= heap[0];
@@ -100,17 +144,119 @@ public:
    void clear() {heap.clear();}
 };
 
-struct Node {
-   int value, 
-      weight,
-      maxvalue;
-   Node()
-   {
-      value = weight = maxvalue = 0;
-   }
-};
+/***********************
+ * HELPER FUNCTIONS
+ **********************/
 
-int main()
+std::vector<std::string> split(const std::string line, 
+                               const std::string split_pattern=",")
+/* PARAMS     : string thing, string separator
+   RETURNS    : vector of strings
+   DESCRIPTION: Splits a string into pieces on `split_pattern` and returns a
+   vector of these results. */
 {
+   std::vector<std::string> ret;
+   int start,
+      end,
+      len = 0;
+
+   for (start = 0; start < line.size(); start += len + 1)
+      {
+      len = 0;
+      end = line.find(split_pattern, start);
+      if (end == std::string::npos)
+         {
+         ret.push_back(line.substr(start, line.size()-start));
+         break;
+         }
+
+      /* If we made it this far, we've got a match. */
+      len = end - start;
+
+      if (len < split_pattern.size())
+         continue;
+
+      ret.push_back(line.substr(start, len));
+
+      }
+   return ret;
+}
+
+unsigned int bounds(vector<Item> items, unsigned int K, unsigned int depth=0)
+/* PARAMS     : a vector of Items, the capacity of the knapsack `K'.
+   RETURNS    : an unsigned int representing the maximum value.
+   DESCRIPTION: Given our items and K, deduce the maximum value bounds by 
+   bounds = currentValue + Sv + (K-TotalSize)*(Vk/Sk)
+*/
+{
+   unsigned int upperBound = 0;
+   unsigned int current_weight = 0;
+   int i;
+   for (i = depth; 
+	i < items.size();
+	i++)
+      {
+
+      current_weight += items[i].weight;
+      upperBound += items[i].value;
+
+      if ((current_weight + items[i+1].weight) > K)
+	 break;
+      }
+
+   i++;
+   
+   // * compute fractional density.
+   if (i < items.size())
+      upperBound += items[i].density() * ( K - current_weight);
+
+   return upperBound;
+}
+
+/***********************
+ * MAIN
+ **********************/
+
+int main(int argc, char **argv)
+{
+   vector<string> args(argv, argc+argv);
+   if (args.size() != 2)
+      {
+      cout << "Usage: " << args[0] << " <weights file>\n";
+      return -1;
+      }
+   ifstream ifile(args[1].c_str());
+
+   if (!ifile)
+      {
+      cout << "Unable to open '" << args[1] << "'.\n";
+      return -1;
+      }
+
+   vector<Item> items;
+
+   string line="";
+   while (std::getline(ifile, line))
+      {
+      Item* i = new Item;
+
+      i->value = atoi(split(line, " ")[0].c_str());
+      i->weight = atoi(split(line, " ")[1].c_str());
+
+      items.push_back(*i);
+      i = NULL;
+      }
+
+   // *sort items by density *
+   std::sort(items.begin(), items.end());
+      
+   for (int i = 0; i < items.size(); i++)
+      cout << i << ": " 
+	   << items[i].weight << "w " 
+	   << items[i].value << "v "
+	   << items[i].density() << "d\n";
+
+   cout << "Upper: " << bounds(items, 16, 1) << endl; 
+
    return 0;
 }
