7c139de35435d76391c85110a138ff4c6b1abeda
meyersh <meyersh@24268613-4aff-4948-b4aa-51f13608ddc2>
2011-03-26 17:27:13 +0000

    More polishing, added all required comments. Time for a break.
    git-svn-id: svn+ssh://gimli.morningside.edu/home/meyersh/svn/src@341 24268613-4aff-4948-b4aa-51f13608ddc2

--- 310/slices/8/knapsack.cpp
+++ 310/slices/8/knapsack.cpp
@@ -1,17 +1,38 @@
 /*
-  Create a standalone application that uses the max heap like the one we created in class to solve instances of the knapsack problem. The max heap (in addition to functioning correctly) should abide by the following conventions:
+  knapsack.cpp (CSCI 310 Slice 8 App 1)
+  CREATED BY: Shaun Meyer
+  CREATED   : MAR 26 2011
+  SUBMITTED BY: Shaun Meyer
+  
+  DESCRIPTION:
+
+  Create a standalone application that uses the max heap like the one we created in
+class to solve instances of the knapsack problem. The max heap (in addition to 
+functioning correctly) should abide by the following conventions:
 
   * The name of the template should be maxHeapPQ
   * At a minimum, it should have the following operations as public functions:
-  o void addItem(const A &item) This adds another item to the end of the heap and then uses sift-up to place it in the proper position.
-  o int readLargest(A &item) This assigns the largest item in the heap to the parameter (item) but does not change the heap. It returns 0 on success, 1 if there is nothing in the heap.
-  o int removeLargest(A &item) This assigns the largest item in the heap to the parameter (item) and then removes the largest items from the heap. It returns 0 on success, 1 if there is nothing in the heap.
+    o void addItem(const A &item) This adds another item to the end of the heap 
+      and then uses sift-up to place it in the proper position.
+  o int readLargest(A &item) This assigns the largest item in the heap to the 
+    parameter (item) but does not change the heap. It returns 0 on success, 1 if 
+    there is nothing in the heap.
+  o int removeLargest(A &item) This assigns the largest item in the heap to the 
+    parameter (item) and then removes the largest items from the heap. It returns 
+    0 on success, 1 if there is nothing in the heap.
   o int isEmpty() returns 1 if the heap is empty, 0 otherwise.
   o clear() clears out the heap so that it is empty.
 
-  Your program should take one parameter, which is the name of the file defining the knapsack instance. This file should have two numbers per line, the first being the value of an item and the second the weight of the item. The numbers should be separated by one or more blanks. You may assume the file is appropriately formatted so that you can use extraction (>>) and not have to check input. You may assume that your program can read until it encounters EOF.
+  Your program should take one parameter, which is the name of the file defining 
+the knapsack instance. This file should have two numbers per line, the first being 
+the value of an item and the second the weight of the item. The numbers should be 
+separated by one or more blanks. You may assume the file is appropriately formatted
+so that you can use extraction (>>) and not have to check input. You may assume 
+that your program can read until it encounters EOF.
 
-  Your program should output the combined value and combined weight of your solution. It should also output the indexes (starting at 0) of the items placed in the knapsack.
+  Your program should output the combined value and combined weight of your 
+solution. It should also output the indexes (starting at 0) of the items placed in 
+the knapsack.
 
 */
 
@@ -22,8 +43,6 @@
 #include <algorithm>
 
 using namespace std;
-
-#define KNAPSACK_SIZE 50
 
 struct Item {
    unsigned int value, weight, max_value, level;
@@ -42,6 +61,9 @@ struct Item {
    }
    
    unsigned int density() const
+   /* PARAMS     : none
+      RETURNS    : unsigned int of value/weight density
+      DESCRIPTION: Simply calculation to reveal item density */
    {
       if (value == 0 || weight == 0)
 	 return 0;
@@ -61,20 +83,17 @@ struct Item {
 	 return true;
       return false;
    }
-   /*
-   bool operator== (const Item &b) const
-   {
-      if (this->density() == b.density())
-	 return true;
-      return false;
-   }
-   */
    
    string selection_str()
+   /* PARAMS     : null
+      RETURNS    : string representation of selection vector
+      DESCRIPTION: The selection vector is a vector of bools, 
+      this converts that to a string for easy cout'ing */
+
    {
       string ret;
       for (int i = 0; i < selection.size(); i++)
-	 ret += selection[i];
+	 ret += selection[i] ? "1" : "0";
       return ret;
    }
 
@@ -85,6 +104,9 @@ private:
    vector<V> heap; // ptr to root of heap 
 
    void swap(V &a, V &b)
+   /* PARAMS     : references to item a and item b
+      RETURNS    : void
+      DESCRIPTION: Swaps the value of a and b. */
    {
       V t = a;
       a = b;
@@ -92,6 +114,9 @@ private:
    }
 
    void sift_up(int i=-1)
+   /* PARAMS     : int representing current node (if we're recursing)
+      RETURNS    : void
+      DESCRIPTION: Recursively sifts up a node to restore heap property. */
    {
       if (i == -1)
 	 i = heap.size()-1;
@@ -108,6 +133,9 @@ private:
    }
 
    void sift_down(int i=0)
+   /* PARAMS     : int representing current node (if we're recursing)
+      RETURNS    : void
+      DESCRIPTION: Recursively sifts down a node to restore heap property. */
    {
       if( i*2 > heap.size()-1 || i*2+1 > heap.size()-1 )
 	 return;
@@ -131,12 +159,19 @@ private:
 
 public:
    void addItem(V &item)
+   /* PARAMS     : an item reference
+      RETURNS    : void
+      DESCRIPTION: add an item to the heap. */
    {
       heap.push_back(item);
       sift_up();
    }
 
-   int readlargest(V &item) {
+   int readlargest(V &item) 
+   /* PARAMS     : an item reference
+      RETURNS    : non-zero on fail
+      DESCRIPTION: Reads the top-of-heap and returns the value by reference. */
+   {
       if (heap.size())
 	 item= heap[0];
       else 
@@ -146,6 +181,9 @@ public:
    }
       
    int removeLargest(V &item)
+   /* PARAMS     :  an item reference
+      RETURNS    : non-zero on fail
+      DESCRIPTION: Reads / removes the top of heap. */
    {
       if (heap.size()==0)
 	 return -1;
@@ -164,8 +202,22 @@ public:
       return 0;
    }
 
-   int isEmpty() {return heap.empty();}
-   void clear() {heap.clear();}
+   int isEmpty() 
+   /* PARAMS     : none
+      RETURNS    : int
+      DESCRIPTION: Wrapper around heap.empty() function. */
+   {
+      return heap.empty();
+   }
+
+
+   void clear() 
+   /* PARAMS     : none
+      RETURNS    : void
+      DESCRIPTION: Clears the contents of the heap. */
+   {
+      heap.clear();
+   }
 };
 
 /***********************
@@ -206,18 +258,14 @@ std::vector<std::string> split(const std::string line,
    return ret;
 }
 
-unsigned int sum_value(vector<Item> items, unsigned int level)
+unsigned int sum_weight(vector<Item> items)
+/* PARAMS     : vector of Items
+   RETURNS    : unsigned int weight
+   DESCRIPTION: Sums the weight of all items in a given vector.
+*/
 {
    unsigned int sum = 0;
-   for (int i = 0; i < items.size() || i < level; i++)
-      sum += items[i].value;
-   return sum;
-}
-
-unsigned int sum_weight(vector<Item> items, unsigned int level)
-{
-   unsigned int sum = 0;
-   for (int i = 0; i < items.size() || i < level; i++)
+   for (int i = 0; i < items.size(); i++)
       sum += items[i].weight;
    return sum;
 }
@@ -259,13 +307,17 @@ unsigned int calculate_bound(vector<Item> items, unsigned int K,
 	 break;
       }
 
-   i++;
-   
-   /*
-   // * compute fractional density.
-   if (i < items.size())
+   /* Computing the fractional desnsity gives us a 
+      bound which is TOO high...
+
+      i++;
+
+      // * compute fractional density.
+      if (i < items.size())
       upperBound += items[i].density() * ( K - current_weight);
+      
    */
+
    return upperBound;
 }
 
@@ -290,13 +342,14 @@ int main(int argc, char **argv)
 {
    /* Handle user parameters */
    vector<string> args(argv, argc+argv);
-   if (args.size() != 2)
+   if (args.size() != 3)
       {
-      cout << "Usage: " << args[0] << " <weights file>\n";
+      cout << "Usage: " << args[0] << " <weights file> <weight limit>\n";
       return -1;
       }
 
    ifstream ifile(args[1].c_str());
+   unsigned int knapsack_size = atoi(args[2].c_str());
 
    if (!ifile)
       {
@@ -304,7 +357,9 @@ int main(int argc, char **argv)
       return -1;
       }
 
-   /* Load up the file */
+   /***********************
+    * Load up the file 
+    ***********************/
 
    vector<Item> items;
 
@@ -320,31 +375,44 @@ int main(int argc, char **argv)
       i = NULL;
       }
 
-   /* Process the file */
+   /***********************
+    * Process the items
+    ***********************/
 
-   // *sort items by density*
+   /*sort items by density (rbegin + rend are "reverse" iterators) */
    std::sort(items.rbegin(), items.rend());
 
    /* print out our items list for debugging */
+   cout << "Inventory: (Sorted by value/density)\n";
    for (int i = 0; i < items.size(); i++)
       cout << i << ": " 
 	   << items[i].value << "v "
 	   << items[i].weight << "w " 
 	   << items[i].density() << "d\n";
+   cout << endl;
 
    maxHeapPQ<Item> prioq; // priority queue
-   int best_so_far=-1;
    unsigned int best_value = 0;
    vector<bool> best_selection; // best selection thus far
-   int weight = 0;
 
+   /* Filter out crazy knapsack's right away */
+   if (sum_weight(items) <= knapsack_size)
+      {
+      cout << "All items will fit in the knapsack neatly.\n";
+      return 0;
+      }
+   else if (items.back().weight > knapsack_size)
+      {
+      cout << "Nothing at all will fit into this knapsack.\n";
+      return 0;
+      }
+   
    /* Initialize before entering while() loop */
-   items[0].max_value = calculate_bound(items, KNAPSACK_SIZE);
    Item initialnode; // this is all zip except for bounds.
-   initialnode.max_value = calculate_bound(items, KNAPSACK_SIZE);
-   prioq.addItem(initialnode);
+   initialnode.max_value = calculate_bound(items, knapsack_size);
 
-   cout << "Initial max_value: " << items[0].max_value << endl;
+   prioq.addItem(initialnode);
+   cout << "Initial max_value: " << initialnode.max_value << endl;
 
    while (!prioq.isEmpty())
       {
@@ -353,7 +421,7 @@ int main(int argc, char **argv)
       prioq.removeLargest(currNode);
 
       if (currNode.max_value > best_value &&
-	  currNode.weight <= KNAPSACK_SIZE) 
+	  currNode.weight <= knapsack_size) 
 	 {
 	 Item next_added = currNode;
 
@@ -364,9 +432,9 @@ int main(int argc, char **argv)
 	 next_added.weight += items[currNode.selection.size()].weight;
 
 	 next_added.max_value = 
-	    calculate_bound(items, KNAPSACK_SIZE, &next_added);
+	    calculate_bound(items, knapsack_size, &next_added);
 
-	 if (next_added.weight <= KNAPSACK_SIZE)
+	 if (next_added.weight <= knapsack_size)
 	    {
 	    if (next_added.value > best_value)
 	       {
@@ -380,7 +448,7 @@ int main(int argc, char **argv)
 	 Item next_not_added = currNode; // inherit weight+value
 	 next_not_added.selection.push_back(false);
 	 next_not_added.max_value = 
-	    calculate_bound(items, KNAPSACK_SIZE, &next_not_added);
+	    calculate_bound(items, knapsack_size, &next_not_added);
 
 	 if (next_not_added.max_value > best_value)
 	    {
@@ -398,8 +466,32 @@ int main(int argc, char **argv)
 	   << "(best sel: " << selection_str(best_selection) << ")\n";
 
       }
+   
    cout << "sel: (" << best_selection.size() << ") '" 
 	<< selection_str(best_selection) << "'\n";
-   cout << "Best so far: " << best_value << endl;
+
+   cout << "\nResulting knapsack inventory:\n";
+   int weight = 0,
+      value = 0;
+
+   for (int i = 0; i < best_selection.size(); i++)
+      {
+      if (best_selection[i] == false)
+	 {
+	 cout << i << ": NOT CHOSEN\n";
+	 continue;
+	 }
+
+      weight += items[i].weight;
+      value += items[i].value;
+
+      cout << i << ": " 
+	   << items[i].value << "v " 
+	   << items[i].weight << "w "
+	   << items[i].density() << "d\n";
+      }
+   cout << "Total weight: " << weight << "/" << knapsack_size << endl
+	<< "Total value:  " << value << endl;
+
    return 0;
 }
