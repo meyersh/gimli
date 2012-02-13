9bf2b983d24c71bfbaa0c97dc2e2623a933b4586
Shaun Meyer <scratch@lapper.(none)>
2012-02-13 14:55:46 -0600

    Boilerplate for p(n)

---
+++ 431/project1/polynomial.hpp
@@ -0,0 +1,16 @@
+#ifndef __POLYNOMIAL_HPP__
+#define __POLYNOMIAL_HPP__
+
+/*
+  http://www.lawrence.edu/fast/greggj/CMSC270/Infix.html
+*/
+
+#include <string>
+
+long long int p(string polynomial, int n)
+/* Parse and evaluate a polynomial contained in the string */
+{
+} 
+
+
+#endif
