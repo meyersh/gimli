14bb0a787e8da69efe2cb42e01d9279ed67c0edb
Shaun Meyer <meyersh@morningside.edu>
2012-03-01 17:04:15 -0600

    Added a bunch of informative comments.

--- 431/project3/project3.cpp
+++ 431/project3/project3.cpp
@@ -5,17 +5,37 @@ using namespace std;
 
 int main() {
 
-   State b;
+   //
+   // States
+   //
+
+   // May be initialized with NO length.
+   State b;     
    b.insert(1);
    b.insert(4);
    b.insert(2);
    b.insert(2);
 
+   // Or we may initialize it with some arbitrary length.
+   State b2(5); 
+   for (int i = 0; i < 5; i++)
+	  b2[i] = i;
+
+   // b2[5] // this would segfault!
+
    cout << b[1] << endl;
    cout << b.toString() << endl;
 
+   //
+   // Weights
+   //
+
+   // Weights always requires a filename.
    Weight w("weights.txt");
-   if (w.load())
+
+   /* Non-zero indicates a file couldn't be loaded. Probably means 
+	  we need to initialize some values. */
+   if (w.load()) 
 	  {
 	  w.insert(1);
 	  w.insert(3);
@@ -26,6 +46,8 @@ int main() {
    cout << w[1] << endl;
    cout << w.toString() << endl;
 
+   /* We haven't saved our weights object! That's OK, the 
+	  destructor calls save() for us. Whew. */
    return 0;
 
 }
